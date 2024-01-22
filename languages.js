564289c9f97653cbf068a963b9c360ff5cd11b02
MusariKeerthana <157366987+MusariKeerthana@users.noreply.github.com>
2024-01-22 16:27:02 +0530

    Add files via upload

---
+++ languages.js
@@ -0,0 +1,134 @@
+const languages = [
+    {
+      no: "0",
+      name: "Auto",
+      native: "Detect",
+      code: "auto",
+    },
+    {
+      no: "3",
+      name: "Arabic",
+      native: "عربي",
+      code: "ar",
+    },
+    {
+      no: "4",
+      name: "Armenian",
+      native: "Հայերէն",
+      code: "hy",
+    },
+   
+    {
+      no: "10",
+      name: "Chinese (Simplified)",
+      native: "中文简体",
+      code: "zh-CN",
+    },
+    {
+      no: "11",
+      name: "Chinese (Traditional)",
+      native: "中文繁體",
+      code: "zh-TW",
+    },
+    {
+      no: "14",
+      name: "Danish",
+      native: "Dansk",
+      code: "da",
+    },
+    {
+      no: "15",
+      name: "Dutch",
+      native: "Nederlands",
+      code: "nl",
+    },
+    {
+      no: "16",
+      name: "English",
+      native: "English",
+      code: "en",
+    },
+    {
+      no: "19",
+      name: "Finnish",
+      native: "Suomi",
+      code: "fi",
+    },
+    {
+      no: "20",
+      name: "French",
+      native: "Français",
+      code: "fr",
+    },
+    {
+      no: "23",
+      name: "German",
+      native: "Deutsch",
+      code: "de",
+    },
+    {
+      no: "24",
+      name: "Greek",
+      native: "Ελληνικά",
+      code: "el",
+    },
+    {
+      no: "27",
+      name: "Hindi",
+      native: "हिन्दी",
+      code: "hi",
+    },
+    {
+      no: "32",
+      name: "Italian",
+      native: "Italiano",
+      code: "it",
+    },
+    {
+      no: "33",
+      name: "Japanese",
+      native: "日本語",
+      code: "ja",
+    },
+    {
+      no: "34",
+      name: "Korean",
+      native: "한국어",
+      code: "ko",
+    },
+    
+   
+    {
+      no: "43",
+      name: "Portuguese",
+      native: "Português",
+      code: "pt",
+    },
+    {
+      no: "44",
+      name: "Romanian",
+      native: "Română",
+      code: "ro",
+    },
+    {
+      no: "45",
+      name: "Russian",
+      native: "Русский",
+      code: "ru",
+    },
+    
+    {
+      no: "49",
+      name: "Spanish",
+      native: "Español",
+      code: "es",
+    },
+   
+    {
+      no: "55",
+      name: "Urdu",
+      native: "اردو",
+      code: "ur",
+    },
+   
+  ];
